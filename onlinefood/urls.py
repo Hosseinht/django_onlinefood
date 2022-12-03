bad13b08bb3e96eb500e65ed95ad626764badbd9
Ryan <sailantht@gmail.com>
2022-12-03 08:57:42 +0330

    Home Page

--- onlinefood/urls.py
+++ onlinefood/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name='home.html')),
+
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
