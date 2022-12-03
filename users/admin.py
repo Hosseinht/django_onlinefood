d24cbe96ada2c4bd28ff9edcfc70d6eba8830858
Ryan <sailantht@gmail.com>
2022-12-03 17:51:15 +0330

    User Model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,83 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class AdminUser(UserAdmin):
+    # model=User
+    list_display = [
+        "id",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+        "is_active",
+    ]
+    list_display_links = ["id", "email", "username"]
+    list_filter = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    ]
+    readonly_fields = ["date_joined", "last_login", "created_date", "modified_date"]
+    fieldsets = (
+        ("Password", {"fields": ("password",)}),
+        (
+            "Personal Information",
+            {
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("User Type", {"fields": ("role",)}),
+        (
+            "Permissions and Groups",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_admin",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates",
+            {"fields": ("last_login", "date_joined", "created_date", "modified_date")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_admin",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, AdminUser)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,96 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
 
-class User(AbstractBaseUser):
-    pass
+class UserManager(BaseUserManager):
+    def create_user(
+        self, email, username, first_name, last_name, password, **extra_fields
+    ):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must an username")
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(
+        self, email, username, first_name, last_name, password, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must be assigned to is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must be assigned to is_superuser=True.")
+        if extra_fields.get("is_admin") is not True:
+            raise ValueError("Superuser must be assigned to is_admin=True.")
+
+        email = self.normalize_email(email)
+        user = self.create_user(
+            email, username, first_name, last_name, password, **extra_fields
+        )
+
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    VENDOR = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (VENDOR, "Vendor"),
+        (CUSTOMER, "Customer"),
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_short_name(self):
+        return self.username
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
